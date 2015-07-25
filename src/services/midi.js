f4162592c403ba51068b92f77457385f74129773
pastelInc <keyaki765@gmail.com>
2015-07-25 14:47:16 +0900

    (faet): add onMidiEvent

--- src/services/midi.js
+++ src/services/midi.js
@@ -5,16 +5,23 @@ const modName = 'app.services.midi';
 class Midi {
   constructor() {
     this.init();
+    this.inputs = [];
   }
 
   init() {
     navigator.requestMIDIAccess()
-      .then(this.onMIDISuccess, this.onMIDIFailure);
-    this.inputs = null;
+      .then(this.onMIDISuccess.bind(this), this.onMIDIFailure.bind(this));
   }
 
   onMIDISuccess(access) {
-    console.log(access);
+    const input = access.inputs.values();
+
+    for (var o = input.next(); !o.done; o = input.next()) {
+      this.inputs.push(o.value);
+    }
+    for (var cnt = 0; cnt < this.inputs.length; cnt++) {
+      this.inputs[cnt].onmidimessage = this.onMIDIEvent;
+    }
   }
 
   onMIDIFailure(msg) {
@@ -23,8 +30,13 @@ class Midi {
   }
 
   onMIDIEvent(e) {
-    console.log("catch midi event");
-    console.log(e);
+    const statusByte = e.data[0].toString(16).substring(0, 1);
+    if (statusByte === "8") {
+      console.log("note off");
+    }
+    if (statusByte === "9") {
+      console.log("note on");
+    }
   }
 }
 
